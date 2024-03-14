4583531969778482a17286757f3dc0d6df913428
Hugo Ramza <l18100746@nlaredo.tecnm.mx>
2024-03-14 11:16:45 -0500

    Server Template

---
+++ Parcial 2/ServerTemplate/ServerStatic/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+ 
+app.set('view engine', 'pug');
+app.set('views',path.join(__dirname,'views'));
+ 
+//Get
+app.get('/', (req, res, next) => {
+    res.render('index.pug');
+    });
+ 
+app.listen(3000, () => {
+console.log('Example app listening on port 3000!');
+});
